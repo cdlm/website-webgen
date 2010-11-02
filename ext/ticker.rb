460a936738b8f98e7c5121c45dbd84a78af04a57
Damien Pollet <damien.pollet@gmail.com>
2010-11-02 19:40:36 +0100

    News ticker selection & ordering
    Distinguish between planified events and simple news posts from the date(s)
    Select past/upcoming events
    Order according to selected subset

--- ext/ticker.rb
+++ ext/ticker.rb
@@ -22,22 +22,47 @@
 module Ticker
 
   # A single news record
-  NewsItem = Struct.new :title, :date, :text
+  NewsItem = Struct.new :title, :dates, :text
   class NewsItem
-    def id
-      "#{title.gsub(/[^\w_-]/, '-')}-#{date}"
+
+    def planified?
+      dates.is_a? Array
     end
 
-    def format_date
-      date.strftime '%d %b %G'
+    def upcoming?
+      on_date > Date.today + 1 # tomorrow is not really upcoming anymore
+    end
+
+    def on_date
+      planified?  ?  dates[0]  :  dates
+    end
+
+    def until_date
+      planified?  ?  dates[-1]  :  dates
+    end
+
+    def format_when
+      if planified?
+        if on_date == until_date
+          "Event on #{dmy(on_date)}"
+        else
+          "Event from #{dmy(on_date)} to #{dmy(until_date)}"
+        end
+      else
+        "Posted on #{dmy(on_date)}"
+      end
     end
 
     def render_on io
       io << <<-EOS
-<h6 title='#{format_date}'>#{title}</h6>
+<h6 title='#{format_when}'>#{title}</h6>
 #{text}
 
 EOS
+    end
+
+    def dmy(date)
+      date.strftime '%d %b %G'
     end
   end
 
@@ -45,8 +70,8 @@ EOS
   # Converting data to more convenient Ruby objects without using fancy YAML syntax
   class News
 
-    def initialize( data, more, chronological, number )
-      @more, @chronological, @number = more, chronological, number
+    def initialize( data, more, chronological, number, period )
+      @more, @chronological, @number, @period = more, chronological, number, period
       @items = load_items data
     end
 
@@ -68,14 +93,22 @@ EOS
     end
 
     def load_items( data )
-      data = data.map { |d|  NewsItem.new d['title'], d['date'], d['text'] }
-      if @chronological # does not make much sense if there are more than @number items...
-        data.sort { |x, y|  x.date <=> y.date }
-      else
-        data.sort { |x, y|  y.date <=> x.date }
+      data = data.map { |d|  NewsItem.new d['title'], d['when'], d['text'] }
+      case @period
+      when :upcoming then data = data.select { |each| each.upcoming? }
+      when :past     then data = data.reject { |each| each.upcoming? }
       end
+      if  @chronological.nil?  &&  @period == :upcoming  ||  @chronological
+      then upsort data else downsort data end
     end
 
+    def upsort data
+      data.sort { |x, y|  x.on_date <=> y.on_date }
+    end
+
+    def downsort data
+      data.sort { |x, y|  y.on_date <=> x.on_date }
+    end
   end
 
 
@@ -94,7 +127,11 @@ EOS
         end
       context.dest_node.node_info[:used_nodes] << news_node.alcn
       # load & display news items
-      news = News.new news_node.meta_info['news'], param('ticker.more'), param('ticker.chronological'), param('ticker.number')
+      news = News.new news_node.meta_info['news'],
+                      param('ticker.more'),
+                      param('ticker.chronological'),
+                      param('ticker.number'),
+                      param('ticker.period')
       result = ""
       unless news.empty?
         StringIO.open result do |io|
@@ -119,9 +156,11 @@ config = Webgen::WebsiteAccess.website.config
 config['contentprocessor.tags.map']['ticker'] = 'Ticker::Tag'
 config.ticker.path nil,
   :doc => 'Path to the node with the news data. Current node if omitted.'
+config.ticker.period :past,
+  :doc => 'Which news to display. Either of :past (default), :upcoming, or :all.'
 config.ticker.number 5,
   :doc => 'Number of entries to display, or ~ (nil). Defaults to 5.'
 config.ticker.more nil,
   :doc => 'Text of the link to all news items. No link displayed if omitted.'
-config.ticker.chronological false,
-  :doc => 'Display items in chronological order rather than from newest to oldest.'
+config.ticker.chronological nil,
+  :doc => 'Display items in chronological order (true), from newest to oldest (false), or according to period (nil, the default).'
